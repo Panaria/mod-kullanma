db2b8686791f262557ec41601d5dd7e1dad4cef7
Panaria <baranyldzby@gmail.com>
2023-10-11 17:10:24 +0300

    Add project files.

---
+++ mod kullanma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod_kullanma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Kullanıcının girdiği sayınının 7'ye ve 5'e tam bölünüp bölünemediğini bulan programı yazınız.
+
+            int sayi;
+
+            Console.WriteLine("Sayı Giriniz");
+            sayi = int.Parse(Console.ReadLine());
+
+            if (sayi % 7 == 0 && sayi % 5 == 0)
+            {
+                Console.WriteLine("Sayı 5'e ve 7'ye tam bölünür.");
+            }
+            else
+            {
+                Console.WriteLine("Sayı 5'e ve 7'ye tam bölünemez.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
